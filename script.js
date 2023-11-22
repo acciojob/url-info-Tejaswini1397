12f3d9169a4f8b735035706e4c3de965f3322783
Tejaswini Khairnar <117677072+Tejaswini1397@users.noreply.github.com>
2023-11-22 09:54:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+const currentURL = window.location.href;
+
+// Get the length of the URL
+const urlLength = currentURL.length;
+alert("The length of the URL is: " + urlLength);
